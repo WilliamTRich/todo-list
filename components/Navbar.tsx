cd74e179abd3a92140c717bffafe1aaded81c63f
WilliamTRich <williamrich@homerak.net>
2023-04-20 13:59:43 -0700

    Worked on navbar, and schema.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,29 @@
 //Utilities
 import React from "react";
-import { signOut } from "next-auth/react";
 
 //Components
+import { signOut } from "next-auth/react";
 
+//Props for NavBar
 interface NavbarProps {
   username: string;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ username }) => {
   return (
-    <nav className="flex flex-col w-56 bg-gray-800 h-full items-center ">
+    <nav className="flex flex-col border-r-black border-r-2 w-56 bg-gray-800 h-full items-center ">
       <div className="flex-grow w-full">
         <h1 className="text-center w-full mt-24 mb-10 font-semibold text-xl text-gray-300 border-b-2 border-gray-600 pb-2">{username}</h1>
-        <div className="flex flex-col gap-8 items-center">
-          <div className="flex w-full gap-4 hover:bg-gray-200 hover:cursor-pointer">
+        <div className="flex flex-col items-center">
+          <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
             <img src="imgs/dashboard.svg" height={'40rem'} width={'40rem'} />
             <h2 className="text-xl align-text-top">Dashboard</h2>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
             <img src="imgs/artifical-intelligence.svg" height={'40rem'} width={'40rem'} />
             <h2 className="text-xl">AI-Generate</h2>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
             <img src="imgs/settings.svg" height={'40rem'} width={'40rem'} />
             <h2 className="text-xl">Settings</h2>
           </div>
