af3e9fec0e53093490e29b561b9daaeef1e8dcb4
William Rich <112077180+WilliamTRich@users.noreply.github.com>
2023-04-27 19:04:18 -0800

    Worked on design for todo component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,29 +9,39 @@ interface NavbarProps {
   username: string;
 }
 const Navbar: React.FC<NavbarProps> = ({ username }) => {
-
   return (
     <nav className="flex flex-col border-r-black border-r-2 w-56 bg-gray-800 h-full items-center ">
       <div className="flex-grow w-full">
-        <h1 className="text-center w-full mt-24 mb-10 font-semibold text-xl text-gray-300 border-b-2 border-gray-600 pb-2">{username}</h1>
+        <h1 className="text-center w-full mt-24 mb-10 font-semibold text-xl text-gray-300 border-b-2 border-gray-600 pb-2">
+          {username}
+        </h1>
         <div className="flex flex-col items-center">
           <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
-            <img src="imgs/dashboard.svg" height={'40rem'} width={'40rem'} />
+            <img src="imgs/dashboard.svg" height={"40rem"} width={"40rem"} />
             <h2 className="text-xl align-text-top">Dashboard</h2>
           </div>
           <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
-            <img src="imgs/artifical-intelligence.svg" height={'40rem'} width={'40rem'} />
+            <img
+              src="imgs/artifical-intelligence.svg"
+              height={"40rem"}
+              width={"40rem"}
+            />
             <h2 className="text-xl">AI-Generate</h2>
           </div>
           <div className="flex w-full h-16 items-center justify-center gap-4 hover:bg-gray-500 hover:cursor-pointer ease-in duration-300">
-            <img src="imgs/settings.svg" height={'40rem'} width={'40rem'} />
+            <img src="imgs/settings.svg" height={"40rem"} width={"40rem"} />
             <h2 className="text-xl">Settings</h2>
           </div>
         </div>
       </div>
-      <button className="mt-auto mb-10 text-white bg-red-600 hover:bg-800 font-bold py-2 px-4 w-2/3 rounded-lg " onClick={() => signOut()}>Log Out</button>
+      <button
+        className="mt-auto mb-10 text-white bg-red-600 hover:bg-800 font-bold py-2 px-4 w-2/3 rounded-lg "
+        onClick={() => signOut()}
+      >
+        Log Out
+      </button>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,9 +1,9 @@
 const Todo = () => {
     return (
-        <div className="flex flex-col h-40 w-80">
-            <div>
-                <input type="text" />
-                <button>+</button>
+        <div className="flex flex-col m-6 p-3 border-2 border-black h-1/3 w-1/3">
+            <div className="flex w-full gap-2 border-b-2 border-gray-500">
+                <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Add Task"/>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">+</button>
             </div>
             <div>
                 <ul>
